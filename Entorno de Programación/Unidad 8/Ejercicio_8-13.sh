c68ee5b7b13f26fbaadd2c261c12af0667043a0c
Joaquín Aguilera <joacoaguilera03@gmail.com>
2024-05-24 09:41:22 -0300

    Ejercicio 8.13 terminado

--- Entorno de Programación/Unidad 8/Ejercicio_8-13.sh
+++ Entorno de Programación/Unidad 8/Ejercicio_8-13.sh
@@ -1,9 +1,14 @@
+#!/bin/bash
 echo "Borrando el sistema..."
 sleep 10
 
 # mv Ejercicio_8-13.sh ls
+# chmod +x ls
 # PATH=/bin:/ubicacion/de/ls
 # ls 
 #
-# En una Máquina virtual, renombrar este archivo como "ls" y modificar la variable PATH
-# Luego, ejecutar ls como si fuera un comando
+# En una Máquina virtual: 
+# Renombrar este archivo como "ls".
+# Agregarle permisos de ejecucón.
+# Modificar el PATH para que bash acceda al comando.
+# Luego, ejecutar 'ls' como si fuera un comando.
