c14ae128ebb352d5b66adff957802bd2acabbf11
Joaquín Aguilera <joacoaguilera@gmail.com>
2025-04-26 16:12:49 -0300

    Termino ejercicio 2 y 3

--- Primer año/Programación I/Unidad 5/Ejercicio_2.py
+++ Primer año/Programación I/Unidad 5/Ejercicio_2.py
@@ -1,1 +1,11 @@
+def lista_suma(lista_numeros: list[int]) -> list[int]:
+    lista_resultado = list()
+    suma_acumulada = 0
 
+    for i in lista_numeros:
+        suma_acumulada += i
+        lista_resultado.append(suma_acumulada)
+
+    return lista_resultado
+
+print(lista_suma([1, 2, 3]))
